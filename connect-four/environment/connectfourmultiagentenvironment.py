edfbe0998c19bc1594c552e4ee9f6af9c47ab95c
Lance Pendragon <lancejabuan@gmail.com>
2025-02-23 19:10:37 -0600

    Initial commit for the connect four multi agent environment

---
+++ connect-four/environment/connectfourmultiagentenvironment.py
@@ -0,0 +1,17 @@
+import gym
+from gym import spaces
+import numpy as np
+from ray.rllib.env.multi_agent_env import MultiAgentEnv
+
+def ConnectFourMultiAgentEnvironment(MultiAgentEnv):
+    def __init(self, config=None):
+        super().__init__()
+
+    def reset():
+        return None
+
+    def step():
+        return None
+
+    def render(self):
+        return None
