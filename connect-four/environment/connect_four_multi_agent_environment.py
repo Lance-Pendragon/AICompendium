b6ea5b7dc8c82e1578d28409af46dae24648b62b
Lance Pendragon <lancejabuan@gmail.com>
2025-02-23 21:07:56 -0600

    fleshing  out the class more, renaming it, adding action masking

---
+++ connect-four/environment/connect_four_multi_agent_environment.py
@@ -0,0 +1,45 @@
+import gym
+from gym import spaces
+import numpy as np
+from ray.rllib.env.multi_agent_env import MultiAgentEnv
+
+
+class ConnectFourMultiAgentEnvironment(MultiAgentEnv):
+    NUM_ROWS = 6
+    NUM_COLUMNS = 7
+
+    def __init__(self, config=None):
+        super().__init__()
+
+        # 0 = empty, 1 = player 1, 2 = player 2
+        self.board = np.zeros((NUM_ROWS, NUM_COLUMNS), dtype=np.int8)
+
+        self.action_space = spaces.Discrete(7)  # 7 columns to pick from
+        self.observation_space = spaces.Dict(
+            {
+                "board": spaces.Box(
+                    low=0, high=2, shape=(NUM_ROWS, NUM_COLUMNS)
+                ),  # connect4 has 6 rows, 7 columns
+                "action_mask": spaces.MultiBinary(NUM_COLUMNS),
+            }
+        )
+
+    def reset(self):
+        return None
+
+    def step(self):
+        return None
+
+    def render(self):
+        return None
+
+    def get_observation_space(self):
+        return spaces.Dict(
+            {
+                "board": spaces.Box(low=0, high=2, shape=(6, 7)),
+                "action_mask": self.get_action_mask(),
+            }
+        )
+
+    def get_action_mask(self):
+        return [int(not self.board[0][col] != 0) for col in range(NUM_COLUMNS)]
